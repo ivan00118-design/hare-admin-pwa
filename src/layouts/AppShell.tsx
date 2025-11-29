7261353515125b944625c8619056c6ef94f5f9b8
ivan00118-design <ivan00118@gmail.com>
2025-11-30 03:55:14 +0800

    Add BottomNav component for mobile navigation

--- src/layouts/AppShell.tsx
+++ src/layouts/AppShell.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import BottomNav from '../components/BottomNav';
 
 export default function AppShell() {
   return (
@@ -16,3 +17,22 @@ export default function AppShell() {
     </div>
   );
 }
+return (
+  <div className="flex h-screen w-screen overflow-hidden bg-gray-50">
+    {/* 桌面版顯示 Sidebar (md:flex 表示中型螢幕以上顯示，手機隱藏) */}
+    <div className="hidden md:flex h-full w-64 flex-col fixed inset-y-0 z-50">
+      <Sidebar />
+    </div>
+
+    {/* 主內容區域調整：手機版不需要左邊距 (md:pl-64) */}
+    <main className="flex-1 flex flex-col h-full w-full md:pl-64 pb-16 md:pb-0"> 
+      {/* pb-16 是為了留空間給手機的 BottomNav */}
+      <div className="flex-1 overflow-y-auto p-4 md:p-8">
+        <Outlet />
+      </div>
+    </main>
+
+    {/* 手機版顯示底部導航 */}
+    <BottomNav />
+  </div>
+);
